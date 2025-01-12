9f2bbb5d672ac5475881e6fd80af8b2afc5d1e34
keerthigkaarthik <120106412+keerthigkaarthik@users.noreply.github.com>
2025-01-12 18:25:59 +0800

    Update settings.py

--- bdebooking/settings.py
+++ bdebooking/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-^v#9b9askj&1_(c7qrr9p)z962!=_vqz%bms_6k^!l=@@31n*4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
